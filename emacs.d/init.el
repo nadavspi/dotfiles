eab3c553793170fd5b0dc8fbe496a38c54881085
Nadav Spiegelman <me@nadav.name>
2014-08-30 10:36:41 -0400

    Move safe theme stuff to the top

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -168,16 +168,3 @@
   :ensure rainbow-delimiters 
   :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
- '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
