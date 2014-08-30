353ede4a0fc68df8a62e6ef725d9195aa4cebb44
Nadav Spiegelman <me@nadav.name>
2014-08-29 20:02:08 -0400

    Add package management stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,50 @@
+;;; Basic settings
+
 ;; Remove scrollbars, menu bars, toolbars
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;;; Package management
+
+;; Set up package archives
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;; Install packages
+(defvar required-packages
+  '(
+    evil
+    magit
+  ) "a list of packages to ensure are installed at launch.")
+
+(require 'cl)
+
+; Check if all packages in required-packages are installed
+(defun packages-installed-p ()
+  (loop for p in required-packages
+    when (not (package-installed-p p)) do (return nil)
+    finally (return t)))
+
+; Install missing packages
+(unless (packages-installed-p)
+  ; Refresh packages
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ; install the missing packages
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(package-initialize)
+
+;;; Key binds
+
+(add-hook 'after-init-hook '(lambda ()
+  (global-set-key (kbd "C-x C-m") 'execute-extended-command)
+))
