d927bffc5ac4547d71a61e1a11aa83f54271f65c
Nadav Spiegelman <me@nadav.name>
2014-09-01 23:29:22 -0400

    Add powerline

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -372,6 +372,15 @@ Including indent-buffer, which should not be called automatically on save."
 (use-package writeroom-mode
   :commands writeroom-mode)
 
+;; Powerline
+(use-package powerline
+  :ensure powerline
+  :init
+  (use-package powerline-evil
+    :ensure powerline-evil
+    :config
+    (powerline-evil-vim-color-theme)))
+
 ;; Company
 (use-package company
   :commands company-mode
