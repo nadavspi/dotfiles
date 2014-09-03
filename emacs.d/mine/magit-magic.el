200a98c88717581e76a1529ba9a930bd1aef4ce0
Nadav Spiegelman <me@nadav.name>
2014-09-03 18:32:04 -0400

    Add Gremove

--- emacs.d/mine/magit-magic.el
+++ emacs.d/mine/magit-magic.el
@@ -1,3 +1,6 @@
+;; Inspired by Lukasz Korecki
+;; http://vimeo.com/90132316
+
 (defgroup magit-magic nil
   "Some improvements for magit"
   :group 'magit)
@@ -17,8 +20,17 @@
                  "git" "checkout --" buffer-file-name)
   (find-file-noselect buffer-file-name))
 
+(defun evil-get-remove-current-file ()
+  (interactive)
+  (start-process "git-remove"
+                 (get-buffer-create "*git-remove*")
+                 "git" "rm" "-f"
+                 buffer-file-name)
+  (kill-buffer))
+
 (evil-leader/set-key "ga" 'evil-git-add-current-file)
 (evil-ex-define-cmd "Gread" 'evil-git-checkout-current-file)
+(evil-ex-define-cmd "Gremove" 'evil-git-remove-current-file)
 (evil-leader/set-key
   "gs" 'magit-status
   "gp" 'magit-push)
