01b424b63dd9afc2601b19887f913a8dbc9b4421
Nadav Spiegelman <me@nadav.name>
2014-09-07 21:48:16 -0400

    Use monospace for company tooltip

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector (vector "#cccccc" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#2d2d2d"))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(evil-operator-comment-key "gc")
  '(fci-rule-color "#515151")
@@ -17,8 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
+ '(company-tooltip ((t (:background "cornsilk" :foreground "black" :family "Input Mono")))))
 ;;; Package management
 
 ;; Set up package archives
