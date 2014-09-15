7b068d51ea19d69157109882bc759b8302f017e4
Nadav Spiegelman <me@nadav.name>
2014-09-14 23:38:18 -0400

    Fix company tooltip font customization

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -19,8 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-tooltip ((t (:background "cornsilk" :foreground "black" :family "Input Mono
-  "))))
+ '(company-tooltip ((t (:background "cornsilk" :foreground "black" :family "Input Mono"))))
  '(nxml-attribute-value ((t (:inherit font-lock-string-face :family "Input Mono"))) t))
 ;;; Package management
 
