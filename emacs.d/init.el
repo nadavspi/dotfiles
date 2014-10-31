fcf591fb40a7da2418976fcb161ff27a06380786
Nadav Spiegelman <me@nadav.name>
2014-10-30 23:37:13 -0400

    Make magit-status full screen

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -362,6 +362,12 @@ Including indent-buffer, which should not be called automatically on save."
   (evil-add-hjkl-bindings magit-log-mode-map 'emacs)
   (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
 
+  ;; full screen magit-status
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
   (evil-set-initial-state 'git-commit-mode 'insert))
 ;; Ag
                                         ;(use-package ag
