6160b8d4a278170e4f035f6033ba366150aaeade
Nadav Spiegelman <me@nadav.name>
2017-01-31 18:15:13 -0500

    Refactor zshrc without oh my zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# git
+alias gst="git status"
+
+# tmux 
+alias ta='tmux attach -t'
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
+
+# directory navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# editing 
+if type nvim > /dev/null 2>&1; then
+  alias vim='nvim'
+fi
---
+++ zsh/exports.zsh
@@ -0,0 +1,5 @@
+export EDITOR="nvim"
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+# No delay for mode change (vi mode)
+export KEYTIMEOUT=1
