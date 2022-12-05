af623cf6b5bc42a2eda4d06ad97cdff42b5c2ef4
Nadav Spiegelman <me@nadav.name>
2022-12-04 20:59:56 -0500

    Add polybar, maybe

---
+++ polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar --config=~/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
