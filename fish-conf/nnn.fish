d674b74e96f48e71fad2b0142c572d5370675df6
Nadav Spiegelman <me@nadav.name>
2023-07-23 00:34:25 -0400

    Configure stuff

--- fish-conf/nnn.fish
+++ fish-conf/nnn.fish
@@ -3,9 +3,10 @@ alias n='nnn'
 set -gx NNN_FCOLORS '0000df310000000000000000'
 set -gx NNN_OPTS 'aeA'
 set -gx NNN_PLUG (string join ';' \
-  'f:fzcd' \
-  'h:fzhist' \
+  'a:personal/ag' \
+  'e:-!sudo -E vim "$nnn"*' \
+  'f:personal/fzfind' \
   'j:autojump' \
-  'o:fzopen' \
+  'o:fzplug' \
   'p:preview-tui' \
 )
