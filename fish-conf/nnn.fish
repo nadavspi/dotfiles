c72a91e57efcd8c794d21bc093fcc051295c8bb3
Nadav Spiegelman <me@nadav.name>
2023-07-22 22:08:08 -0400

    Add n

--- fish-conf/nnn.fish
+++ fish-conf/nnn.fish
@@ -1,10 +1,11 @@
 alias ls='nnn'
+alias n='nnn'
 set -gx NNN_FCOLORS '0000df310000000000000000'
 set -gx NNN_OPTS 'aeA'
 set -gx NNN_PLUG (string join ';' \
   'f:fzcd' \
   'h:fzhist' \
-  'j:autojumpp' \
+  'j:autojump' \
   'o:fzopen' \
   'p:preview-tui' \
 )
