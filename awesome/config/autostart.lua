91a4519544cd9986fd35c2ae1fd79fbe94292007
Nadav Spiegelman <me@nadav.name>
2023-08-09 20:25:35 -0400

    Add gnome-keyring

--- awesome/config/autostart.lua
+++ awesome/config/autostart.lua
@@ -1,23 +1,24 @@
 local awful = require("awful")
 
 local function run(command, pidof)
-    local findme = command
-    local firstspace = command:find(' ')
-    if firstspace then
-        findme = command:sub(0, firstspace - 1)
-    end
+	local findme = command
+	local firstspace = command:find(' ')
+	if firstspace then
+		findme = command:sub(0, firstspace - 1)
+	end
 
-    awful.spawn.easy_async_with_shell(string.format('pgrep -u $USER -x %s > /dev/null || (%s)', pidof or findme, command))
+	awful.spawn.easy_async_with_shell(string.format('pgrep -u $USER -x %s > /dev/null || (%s)', pidof or findme, command))
 end
 
 
 local commands = {
-    "picom --experimental-backends &",
-    "blueman-applet &",
-	 "fcitx5 &",
-    -- "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &",
+	"dbus-update-activation-environment --all",
+	"gnome-keyring-daemon --start --components=secrets",
+	"picom --experimental-backends &",
+	"blueman-applet &",
+	"fcitx5 &",
 }
 
 for _, command in ipairs(commands) do
-    run(command)
+	run(command)
 end
