f3b8b347f56fff9ad5aea818ce49a8e639283b52
Nadav Spiegelman <me@nadav.name>
2023-08-18 22:10:02 -0400

    oops

--- zsh/conf.d/fzf.zsh
+++ zsh/conf.d/fzf.zsh
@@ -1,8 +1,7 @@
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'
-  --bind 'space:become(vim {})'"
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
 fe() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
