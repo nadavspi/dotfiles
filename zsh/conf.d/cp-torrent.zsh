d4d1208218c1f4dff1d8abae94e68d092a60cc36
Nadav Spiegelman <me@nadav.name>
2023-08-21 23:05:55 -0400

    Better

--- zsh/conf.d/cp-torrent.zsh
+++ zsh/conf.d/cp-torrent.zsh
@@ -6,5 +6,5 @@ function cp-torrent () {
     echo "Usage: cp-torrent <filename>"
     return 1
   fi
-  rsync -avz $1 seedbox:~/watch/deluge
+  rsync -avz --remove-source-files $@ seedbox:~/watch/deluge
 }
