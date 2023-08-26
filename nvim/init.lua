b67919836f6180bc87d8e0eda41a235bf19d51a2
Nadav Spiegelman <me@nadav.name>
2023-08-26 01:36:32 -0400

    Add cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 -- core options
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.backspace = indent, eol, start
 vim.opt.visualbell = true
 vim.opt.undofile = true
 vim.opt.autoread = true
@@ -52,6 +51,109 @@ lazy.setup({
 	{ "wellle/targets.vim" },
 	{ "kana/vim-textobj-indent", dependencies = { "kana/vim-textobj-user" } },
 	{ "jceb/vim-textobj-uri", dependencies = { "kana/vim-textobj-user" } },
+	{ "neovim/nvim-lspconfig" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-calc",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+      local luasnip = require("luasnip")
+			vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- they way you will only jump inside the snippet region
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<cr>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "luasnip" },
+					{ name = "calc" },
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+				formatting = {
+					format = require("lspkind").cmp_format({
+						mode = "text",
+						menu = {
+							nvim_lsp = "[LSP]",
+							calc = "[Calc]",
+							nvim_lua = "[Lua]",
+							path = "[Path]",
+							buffer = "[Buffer]",
+							omni = "[Omni]",
+						},
+					}),
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
+	{ "onsails/lspkind-nvim" },
 	{
 		"mhartington/formatter.nvim",
 		config = function()
@@ -129,14 +231,14 @@ lazy.setup({
 	{ "akinsho/toggleterm.nvim", lazy = true, version = "*", config = true },
 	{
 		"lmburns/lf.nvim",
-		keys = "-",
+		keys = "<leader>-",
 		config = function()
 			-- This feature will not work if the plugin is lazy-loaded
 			require("lf").setup({
 				border = "rounded",
 			})
 
-			vim.keymap.set("n", "-", "<Cmd>Lf<CR>")
+			vim.keymap.set("n", "<leader>-", "<Cmd>Lf<CR>")
 		end,
 		dependencies = { "toggleterm.nvim" },
 	},
