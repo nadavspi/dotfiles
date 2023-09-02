227364f06ebb2f0e9e001f5702329d5a8bedda61
Nadav Spiegelman <me@nadav.name>
2023-09-01 20:27:15 -0400

    Add leap operator keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -417,6 +417,8 @@ lazy.setup({
 		"ggandor/leap.nvim",
 		config = function()
 			require("leap").add_default_mappings()
+      vim.keymap.set("o", "z", "<Plug>(leap-forward-to)")
+      vim.keymap.set("o", "Z", "<Plug>(leap-backward-to)")
 		end,
 	},
 	{ "knubie/vim-kitty-navigator" },
