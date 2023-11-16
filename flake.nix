8e841689a6cf2aff50192eec66ecc9383eca7be4
Nadav Spiegelman <me@nadav.name>
2023-11-16 18:30:01 -0500

    fleek: apply

--- flake.nix
+++ flake.nix
@@ -1,47 +1,57 @@
-# home-manager switch --flake .#nadavspi
 {
-  description = "nadavspi/dotfiles";
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+  description = "Fleek Configuration";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+
+    # Home manager
+    home-manager.url = "https://flakehub.com/f/nix-community/home-manager/0.1.tar.gz";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Fleek
+    fleek.url = "https://flakehub.com/f/ublue-os/fleek/*.tar.gz";
+
+    # Overlays
+    
+
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-  }:
+  outputs = { self, nixpkgs, home-manager, fleek, ... }@inputs: {
     
-   let 
-     mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
-       pkgs = nixpkgs.legacyPackages.${args.system or "x86_64-linux"};
-       modules = [
-         ./home-manager/home.nix
-         ./home-manager/dotfiles.nix
-         { 
-           home = rec {
-             username = "nadavspi";
-             homeDirectory = "/home/${username}";
-           };
-         }
-       ];
-     } // args);
+     packages.x86_64-linux.fleek = fleek.packages.x86_64-linux.default;
+    
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    
+    homeConfigurations = {
+    
+      "nadavspi@brooklyn" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./brooklyn/nadavspi.nix
+          ./brooklyn/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.x86_64-linux.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
 
-   systems = [
-      { system = "x86_64-linux"; }
-      { system = "x86_64-darwin"; }
-    ];
-
-   in {
-   homeConfigurations.server = mkHomeConfiguration {
-     extraSpecialArgs = rec {
-       withGUI = false;
-       username = "nadavspi";
-       homeDirectory = "/home/${username}";
-       dotfiles = "${homeDirectory}/src/dotfiles";
-     };
-   };
+        ];
+      };
+      
+    };
   };
 }
---
+++ home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, misc, ... }: {
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+      
+      
+    };
+  };
+
+  
+  # managed by fleek, modify ~/.fleek.yml to change installed packages
+  
+  # packages are just installed (no configuration applied)
+  # programs are installed and configuration applied to dotfiles
+  home.packages = [
+    # user selected packages
+    pkgs.bat
+    pkgs.btop
+    pkgs.delta
+    pkgs.eza
+    pkgs.fzf
+    pkgs.gnumake
+    pkgs.just
+    pkgs.lazygit
+    pkgs.lf
+    pkgs.ncdu
+    pkgs.ncurses
+    pkgs.neovim
+    pkgs.python3
+    pkgs.ripgrep
+    pkgs.speedtest-cli
+    pkgs.starship
+    pkgs.tmux
+    pkgs.tree
+    pkgs.wget
+    pkgs.yt-dlp
+    pkgs.zoxide
+    pkgs.zsh
+    pkgs.zstd
+    # Fleek Bling
+    pkgs.git
+    pkgs.htop
+    pkgs.github-cli
+    pkgs.glab
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.vscode
+    pkgs.just
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+  fonts.fontconfig.enable = true; 
+  home.stateVersion =
+    "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
+  programs.home-manager.enable = true;
+}
