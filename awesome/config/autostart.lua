994ccb47d17b7400316cabdc67b2a6637e2b59e7
Nadav Spiegelman <me@nadav.name>
2023-11-21 21:44:06 -0500

    Remove picom

--- awesome/config/autostart.lua
+++ awesome/config/autostart.lua
@@ -15,7 +15,6 @@ local commands = {
 	"dbus-update-activation-environment --all",
 	"/usr/libexec/polkit-gnome-authentication-agent-1 &",
 	"gnome-keyring-daemon --start --components=secrets",
-	"picom --experimental-backends &",
 	"blueman-applet &",
 	"nm-applet &",
 	"fcitx5 &",
