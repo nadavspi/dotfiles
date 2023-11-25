2c07a1f5faf4715dc0639352b2a00e94ba87f180
Nadav Spiegelman <me@nadav.name>
2023-11-24 20:25:00 -0500

    Add client rules

--- awesome/config/rules.lua
+++ awesome/config/rules.lua
@@ -3,44 +3,58 @@ local ruled = require("ruled")
 
 ruled.client.connect_signal("request::rules", function()
 	-- All clients will match this rule.
-	ruled.client.append_rule {
-		id         = "global",
-		rule       = { },
+	ruled.client.append_rule({
+		id = "global",
+		rule = {},
 		properties = {
-			focus     = awful.client.focus.filter,
-			raise     = true,
-			screen    = awful.screen.preferred,
-			placement = awful.placement.no_overlap+awful.placement.no_offscreen
+			focus = awful.client.focus.filter,
+			raise = true,
+			screen = awful.screen.preferred,
+			placement = awful.placement.no_overlap + awful.placement.no_offscreen,
 		},
-		callback = awful.client.setslave
-	}
+		callback = awful.client.setslave,
+	})
 
-	ruled.client.append_rule {
-		id       = "floating",
+	ruled.client.append_rule({
+		id = "floating",
 		rule_any = {
 			instance = { "copyq", "pinentry" },
-			class    = {
-				"Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
-				"Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
+			class = {
+				"Arandr",
+				"Blueman-manager",
+				"Gpick",
+				"Kruler",
+				"Sxiv",
+				"Tor Browser",
+				"Wpa_gui",
+				"veromix",
+				"xtightvncviewer",
 			},
-			name    = {
-				"Event Tester",  -- xev.
+			name = {
+				"Event Tester", -- xev.
 			},
 		},
-		properties = { floating = true }
-	}
-
-
--- 	ruled.client.append_rule {
--- 		rules_any = { class = "firefox"     },
--- 		properties = { tag = "web" }
--- 	}
--- 	ruled.client.append_rule {
--- 		rules_any = { class = "Obsidian" },
--- 		properties = { tag = "notes" }
--- 	}
--- 	ruled.client.append_rule {
--- 		rules_any	= { class = "discord" },
--- 		properties = { tag = "chat" }
--- 	}
+		properties = { floating = true },
+	})
 end)
+
+ruled.client.append_rule({
+	rule = { class = "firefox" },
+	properties = { tag = "firefox" },
+})
+ruled.client.append_rule({
+	rule = { class = "discord" },
+	properties = { tag = "discord" },
+})
+ruled.client.append_rule({
+	rule = { class = "obsidian" },
+	properties = { tag = "notes" },
+})
+ruled.client.append_rule({
+	rule = { class = "mpv" },
+	properties = { tag = "mpv" },
+})
+ruled.client.append_rule({
+	rule = { class = "Anki" },
+	properties = { tag = "anki" },
+})
