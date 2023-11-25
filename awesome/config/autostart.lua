121b2a6b4ea7701d2121e65dc0212fb5253c6729
Nadav Spiegelman <me@nadav.name>
2023-11-24 20:09:48 -0500

    Oops, need dbus-update-activation-environment

--- awesome/config/autostart.lua
+++ awesome/config/autostart.lua
@@ -3,6 +3,7 @@ local awful = require("awful")
 awful.spawn.with_shell(
 	'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;'
 		.. 'xrdb -merge <<< "awesome.started:true";'
+		.. 'dbus-update-activation-environment --all;'
 		.. "flatpak run org.fcitx.Fcitx5 &;"
 		.. "~/src/dotfiles/lock/init.sh &;"
 		.. "dex --environment Awesome --autostart"
