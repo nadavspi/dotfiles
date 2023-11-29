ab5f58e774c50a7ae6e4d5829bb71738c462b6f9
Nadav Spiegelman <me@nadav.name>
2023-11-29 18:30:30 -0500

    Use hostname for adguard web

--- nixos/common/caddy.nix
+++ nixos/common/caddy.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, ... }: {
   services.caddy = {
     enable = true;
-    virtualHosts."192.168.1.202" = {
+    virtualHosts."prague.nadav.is" = {
       extraConfig = ''
         reverse_proxy http://192.168.1.202:3000
         tls internal
