8fd1c4160a189c6643b11aa7c75097d5107cc911
Nadav Spiegelman <me@nadav.name>
2023-12-02 15:26:14 -0500

    Update

--- fish/functions/cp-epub.fish
+++ fish/functions/cp-epub.fish
@@ -1,4 +1,4 @@
 function cp-epub
-  mv -v ~/Downloads/*.epub /mnt/nfs/data/watch/books/
-  mv -v ~/Downloads/*.azw3 /mnt/nfs/data/watch/books/
+  mv -v ~/Downloads/*.epub /mnt/data/watch/books/
+  mv -v ~/Downloads/*.azw3 /mnt/data/watch/books/
 end
