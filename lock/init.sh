e693f574c02a78103a65c647aabcd8725c4d675f
Nadav Spiegelman <me@nadav.name>
2023-12-02 19:05:58 -0500

    Switch lockscreen prompt

--- lock/init.sh
+++ lock/init.sh
@@ -1,5 +1,5 @@
 export XSECURELOCK_FONT="Hack Nerd Font:size=18" 
-export XSECURELOCK_PASSWORD_PROMPT="disco"
+export XSECURELOCK_PASSWORD_PROMPT="asterisks"
 export XSECURELOCK_SHOW_KEYBOARD_LAYOUT=0
 export XSECURELOCK_SAVER=~/src/dotfiles/lock/saved_feh
 export XSECURELOCK_AUTH_BACKGROUND_COLOR="#a0785a"
