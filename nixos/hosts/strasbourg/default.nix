7fd9e7c1677758d64de142f7b8b2b2e7c0dcecb2
Nadav Spiegelman <me@nadav.name>
2023-12-05 20:00:45 -0500

    Add pi

--- nixos/hosts/strasbourg/default.nix
+++ nixos/hosts/strasbourg/default.nix
@@ -1,11 +1,7 @@
-{ inputs, overlays, lib, config, pkgs, ... }: {
-  imports = [ 
-    ./hardware-configuration.nix 
-    ../../common/global
-  ];
+{ ... }: {
+  imports = [ ./hardware-configuration.nix ../../common/global ];
 
   networking.hostName = "strasbourg";
-  networking.networkmanager.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
--- nixos/hosts/strasbourg/hardware-configuration.nix
+++ nixos/hosts/strasbourg/hardware-configuration.nix
@@ -1,4 +1,6 @@
 { ... }: {
   imports =
     [ <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-raspberrypi4.nix> ];
+
+  networking.wireless.enable = false;
 }
