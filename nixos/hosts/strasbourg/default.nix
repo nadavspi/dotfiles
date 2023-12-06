038820014ea929170fa9dd1e647365f96b7b88a0
Nadav Spiegelman <me@nadav.name>
2023-12-05 20:16:41 -0500

    Bring in output of nixos-generate-config

--- nixos/hosts/strasbourg/default.nix
+++ nixos/hosts/strasbourg/default.nix
@@ -1,6 +1,9 @@
 { ... }: {
   imports = [ ./hardware-configuration.nix ../../common/global ];
 
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
   networking.hostName = "strasbourg";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
