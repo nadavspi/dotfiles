13eb02adf7c7eb1d3d771ab8cbdec76f6a9e3d18
Nadav Spiegelman <me@nadav.name>
2023-12-07 23:19:32 -0500

    Add nadavspi.cachix

--- nixos/common/global/cache.nix
+++ nixos/common/global/cache.nix
@@ -2,10 +2,12 @@
   nix.settings.substituters = [
     "https://cache.nixos.org?priority=10"
     "https://nix-community.cachix.org"
+    "https://nadavspi.cachix.org"
   ];
 
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "nadavspi.cachix.org-1:RWw/jQTzs9mbgXNzL4ZKbEdqqlJ4dGoK9pF911QcUbk="
   ];
 }
