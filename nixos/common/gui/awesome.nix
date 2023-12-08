546fd04f59634612fc2ae45e115415dcc59036a5
Nadav Spiegelman <me@nadav.name>
2023-12-07 22:05:32 -0500

    Config keyring and polkit

--- nixos/common/gui/awesome.nix
+++ nixos/common/gui/awesome.nix
@@ -38,13 +38,30 @@ in {
     _1password-gui
     feh
     flameshot
+    gnome.gnome-keyring
     kitty
-    lxde.lxsession
     mpv
     pasystray
     pcmanfm
+    polkit_gnome
     redshift
     rofi
     xfce.xfce4-power-manager
   ];
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
 }
