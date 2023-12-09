249e5bc236ac884ed1a198ad586a8fcb5d0c6a53
Nadav Spiegelman <me@nadav.name>
2023-12-08 23:25:51 -0500

    Move keepalived out

--- nixos/hosts/prague/default.nix
+++ nixos/hosts/prague/default.nix
@@ -6,6 +6,7 @@
     ../../modules/services/adguard.nix
     ../../modules/services/caddy.nix
     ../../modules/services/unbound.nix
+    ./keepalived.nix
   ];
 
   networking.hostName = "prague";
@@ -14,17 +15,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.firewall.extraCommands = "iptables -A INPUT -p vrrp -j ACCEPT";
-  services.keepalived = {
-    enable = true;
-    vrrpInstances.dns = {
-      interface = "enp1s0";
-      state = "MASTER";
-      priority = 50;
-      virtualIps = [{addr = "192.168.1.200";}];
-      virtualRouterId = 200;
-    };
-  };
 
   nadavspi = {
     adguard.enable = true;
---
+++ nixos/hosts/prague/keepalived.nix
@@ -0,0 +1,13 @@
+{...}: {
+  networking.firewall.extraCommands = "iptables -A INPUT -p vrrp -j ACCEPT";
+  services.keepalived = {
+    enable = true;
+    vrrpInstances.dns = {
+      interface = "enp1s0";
+      state = "MASTER";
+      priority = 50;
+      virtualIps = [{addr = "192.168.1.200";}];
+      virtualRouterId = 200;
+    };
+  };
+}
