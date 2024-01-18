e83f4d480edae8d63ee953102fe65b157b521bfe
Nadav Spiegelman <me@nadav.name>
2024-01-17 22:44:59 -0500

    Call dex-autostart
    That's what the rpm calls it

--- home/dot_config/awesome/config/autostart.lua
+++ home/dot_config/awesome/config/autostart.lua
@@ -4,6 +4,6 @@ awful.spawn.with_shell(
 	'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;'
 		.. 'xrdb -merge <<< "awesome.started:true";'
 		.. 'dbus-update-activation-environment --all;'
-		.. "~/src/dotfiles/lock/init.sh &;"
-		.. "dex --environment Awesome --autostart"
+		-- .. "~/src/dotfiles/lock/init.sh &;"
+		.. "dex-autostart --environment Awesome --autostart"
 )
