2289af9ad4891119835d9878d57fac56378496ec
Nadav Spiegelman <me@nadav.name>
2024-02-09 09:59:23 -0500

    Fix background mode

--- home/dot_config/sway/executable_swaybg.sh
+++ home/dot_config/sway/executable_swaybg.sh
@@ -8,7 +8,7 @@ fi
 
 while true; do
     PID=$(pidof swaybg)
-    swaybg -c "#313244" -i "$(find /usr/share/wallpapers/ -type f | shuf -n1)" -m center &
+    swaybg -c "#313244" -i "$(find /usr/share/wallpapers/ -type f | shuf -n1)" -m fit &
     sleep 1
     # shellcheck disable=SC2086
     kill $PID
