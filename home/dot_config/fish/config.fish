060176500a854ea80156cfc731fee446f56d16ea
Nadav Spiegelman <me@nadav.name>
2024-02-12 02:10:40 -0500

    Fish things

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -2,8 +2,5 @@ if status is-interactive
     set fish_greeting
     fish_config theme choose catppuccin-mocha
 
-    function multicd
-        echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
-    end
-    abbr --add dotdot --regex '^\.\.+$' --function multicd
+    zoxide init fish | source
 end
