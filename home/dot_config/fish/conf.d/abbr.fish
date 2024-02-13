6f0e378c40a4a0a0bfa82c090bf2f44203c811a1
Nadav Spiegelman <me@nadav.name>
2024-02-12 19:58:09 -0500

    More fish things

---
+++ home/dot_config/fish/conf.d/abbr.fish
@@ -0,0 +1,10 @@
+abbr -a -- gst "git status"
+abbr -a -- lg lazygit
+abbr -a -- ros "rpm-ostree"
+abbr -a -- v nvim
+abbr -a --position anywhere -- F "| fzf"
+abbr -a --position anywhere -- L "| bat"
+abbr -a --regex ^\\.\\.+\$ --function multicd -- dotdot
+abbr -a --set-cursor="%" -- cp-epub "mv -v ~/Downloads/*.epub% /mnt/data/watch/books/"
+abbr -a --set-cursor="%" -- cp-torrent "rsync -avz --remove-source-files % seedbox:~/watch/deluge"
+abbr -a --set-cursor="%" -- gro "git reset --hard origin/%"
