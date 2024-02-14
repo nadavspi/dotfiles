6a9f53a9c8df3448f3cdb9705fc335cb7fc92b27
Nadav Spiegelman <me@nadav.name>
2024-02-13 21:18:15 -0500

    Add prettierd for astro

--- home/dot_config/nvim/lua/plugins/formatters.lua
+++ home/dot_config/nvim/lua/plugins/formatters.lua
@@ -7,16 +7,19 @@ local function format()
 	end)
 end
 
+-- :help conform-formatters
+-- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
 return {
 	"stevearc/conform.nvim",
 	cmd = { "ConformInfo" },
-	mason_dependencies = { "stylua", "shellcheck", "shfmt" },
+	mason_dependencies = { "stylua", "shellcheck", "shfmt", "prettierd" },
 	keys = {
 		{ "<leader>f", format, mode = { "n", "x" }, desc = "Format buffer" },
 	},
 	opts = {
 		formatters_by_ft = {
-			just = { "just " },
+			astro = {  "prettierd" },
+			just = { "just" },
 			lua = { "stylua" },
 			nix = { "alejandra" },
 			sh = { "shellcheck", "shfmt" },
