a114b3d41d25d604542ecefb70ce5ad9d8a56b42
Nadav Spiegelman <me@nadav.name>
2024-02-23 20:33:44 -0500

    Start tmux when fish starts

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
+and not set -q TMUX
+    exec tmux new -A -s main
+end
+
+if status is-interactive
     set fish_greeting
     fish_config theme choose catppuccin-mocha
     set -g EDITOR nvim
