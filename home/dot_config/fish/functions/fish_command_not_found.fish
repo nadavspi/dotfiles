34ee943356e475f24f848d518bd009cf5a314b0b
Nadav Spiegelman <me@nadav.name>
2024-10-08 18:13:48 -0400

    Remove distrobox stuff

--- home/dot_config/fish/functions/fish_command_not_found.fish
+++
@@ -1,8 +0,0 @@
-function fish_command_not_found
-    # Are we in a container?
-    if test -e /run/.containerenv -o -e /.dockerenv
-        distrobox-host-exec $argv
-    else
-        __fish_default_command_not_found_handler $argv
-    end
-end
